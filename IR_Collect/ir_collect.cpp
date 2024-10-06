80ae825a3eba80db4fdca3801b9941c39ee86573
greggregbean <zininivan2512@gmail.com>
2024-10-06 18:57:43 +0300

    made loggers for load, store, cast (but still can't get string representation of Type)

--- IR_Collect/ir_collect.cpp
+++ IR_Collect/ir_collect.cpp
@@ -15,7 +15,9 @@ namespace {
     IrCollector() : FunctionPass(ID) {}
 
     bool is_logger(StringRef name) {
-      return name == "bin_op_logger" || name == "call_logger" ||name == "func_start_logger" || name == "func_end_logger";
+      return name == "bin_op_logger"     || name == "call_logger"     ||
+             name == "func_start_logger" || name == "func_end_logger" ||
+             name == "load_logger"       || name == "store_logger";
     }
 
     virtual bool runOnFunction(Function& F) {
@@ -32,7 +34,7 @@ namespace {
       //--------------------------------------------------------------------------
       // Prepare call to func_start_logger
       //--------------------------------------------------------------------------
-      ArrayRef<Type*> func_start_param_types = {builder.getInt8Ty()->getPointerTo()};
+      ArrayRef<Type*> func_start_param_types = {builder.getInt8Ty()->getPointerTo()}; // func_name
 
       FunctionType* func_start_log_type = FunctionType::get(void_ret_type, func_start_param_types, false);
       FunctionCallee func_start_log = F.getParent()->getOrInsertFunction("func_start_logger", func_start_log_type);
@@ -40,7 +42,8 @@ namespace {
       //--------------------------------------------------------------------------
       // Prepare call to func_end_logger
       //--------------------------------------------------------------------------
-      ArrayRef<Type*> func_end_param_types = {builder.getInt8Ty()->getPointerTo(), Type::getInt64Ty(ctx)};
+      ArrayRef<Type*> func_end_param_types = {builder.getInt8Ty()->getPointerTo(), // func_name
+                                              Type::getInt64Ty(ctx)};              // inst_addr
 
       FunctionType* func_end_log_type = FunctionType::get(void_ret_type, func_end_param_types, false);
       FunctionCallee func_end_log = F.getParent()->getOrInsertFunction("func_end_logger", func_end_log_type);
@@ -48,9 +51,9 @@ namespace {
       //--------------------------------------------------------------------------
       // Prepare call to call_logger
       //--------------------------------------------------------------------------
-      ArrayRef<Type*> call_param_types = {builder.getInt8Ty()->getPointerTo(),
-                                           builder.getInt8Ty()->getPointerTo(),
-                                           Type::getInt64Ty(ctx)};
+      ArrayRef<Type*> call_param_types = {builder.getInt8Ty()->getPointerTo(), // func_name
+                                          builder.getInt8Ty()->getPointerTo(), // callee_name
+                                          Type::getInt64Ty(ctx)};              // inst_addr
 
       FunctionType* call_log_type = FunctionType::get(void_ret_type, call_param_types, false);
       FunctionCallee call_log = F.getParent()->getOrInsertFunction("call_logger", call_log_type);
@@ -58,20 +61,56 @@ namespace {
       //--------------------------------------------------------------------------
       // Prepare call to bin_op_logger
       //--------------------------------------------------------------------------
-      ArrayRef<Type*> bin_op_param_types = {Type::getInt32Ty(ctx),
-                                            Type::getInt32Ty(ctx),
-                                            Type::getInt32Ty(ctx),
-                                            builder.getInt8Ty()->getPointerTo(),
-                                            builder.getInt8Ty()->getPointerTo(),
-                                            Type::getInt64Ty(ctx)};
+      ArrayRef<Type*> bin_op_param_types = {builder.getInt8Ty()->getPointerTo(), // func_name
+                                            Type::getInt32Ty(ctx),               // val
+                                            Type::getInt32Ty(ctx),               // lhs
+                                            Type::getInt32Ty(ctx),               // rhs
+                                            builder.getInt8Ty()->getPointerTo(), // op_name
+                                            Type::getInt64Ty(ctx)};              // inst_addr
 
       FunctionType* bin_op_log_type = FunctionType::get(void_ret_type, bin_op_param_types, false);
       FunctionCallee bin_op_log = F.getParent()->getOrInsertFunction("bin_op_logger", bin_op_log_type);
 
       //--------------------------------------------------------------------------
+      // Prepare call to load_logger
+      //--------------------------------------------------------------------------
+      ArrayRef<Type*> load_param_types = {builder.getInt8Ty()->getPointerTo(), // func_name
+                                          Type::getInt32Ty(ctx),               // val
+                                          Type::getInt32Ty(ctx),               // p_operand
+                                          builder.getInt8Ty()->getPointerTo(), // load_name
+                                          Type::getInt64Ty(ctx)};              // inst_addr
+
+      FunctionType* load_log_type = FunctionType::get(void_ret_type, load_param_types, false);
+      FunctionCallee load_log = F.getParent()->getOrInsertFunction("load_logger", load_log_type);
+
+      //--------------------------------------------------------------------------
+      // Prepare call to store_logger
+      //--------------------------------------------------------------------------
+      ArrayRef<Type*> store_param_types = {builder.getInt8Ty()->getPointerTo(), // func_name
+                                           Type::getInt32Ty(ctx),               // v_operand
+                                           Type::getInt32Ty(ctx),               // p_operand
+                                           builder.getInt8Ty()->getPointerTo(), // store_name
+                                           Type::getInt64Ty(ctx)};              // inst_addr
+
+      FunctionType* store_log_type = FunctionType::get(void_ret_type, store_param_types, false);
+      FunctionCallee store_log = F.getParent()->getOrInsertFunction("store_logger", store_log_type);
+
+      //--------------------------------------------------------------------------
+      // Prepare call to cast_logger
+      //--------------------------------------------------------------------------
+      ArrayRef<Type*> cast_param_types = {builder.getInt8Ty()->getPointerTo(), // func_name
+                                          Type::getInt32Ty(ctx),               // val
+                                          Type::getInt32Ty(ctx),               // operand
+                                          builder.getInt8Ty()->getPointerTo(), // cast_name
+                                          Type::getInt64Ty(ctx)};              // inst_addr
+
+      FunctionType* cast_log_type = FunctionType::get(void_ret_type, cast_param_types, false);
+      FunctionCallee cast_log = F.getParent()->getOrInsertFunction("cast_logger", cast_log_type);
+
+      //--------------------------------------------------------------------------
       // Start inserting calls
       //--------------------------------------------------------------------------
-      //Insert call to func_start_logger in the function begin
+      // Insert call to func_start_logger in the function begin
       BasicBlock& entry_bb = F.getEntryBlock();
       builder.SetInsertPoint(&entry_bb.front());
 
@@ -81,7 +120,7 @@ namespace {
 
       for (auto& B : F) {
         for (auto& I : B) {
-          Value* insn_addr = ConstantInt::get(builder.getInt64Ty(), (int64_t)(&I));
+          Value* inst_addr = ConstantInt::get(builder.getInt64Ty(), (int64_t)(&I));
 
           // Insert call to call_logger() before CALL
           if (auto* call = dyn_cast<CallInst>(&I)) {
@@ -91,32 +130,77 @@ namespace {
             if (callee && !is_logger(callee->getName())) {
               Value* callee_name = builder.CreateGlobalStringPtr(callee->getName());
               Value* func_name = builder.CreateGlobalStringPtr(F.getName());
-              Value* args[] = {func_name, callee_name, insn_addr};
+              Value* args[] = {func_name, callee_name, inst_addr};
               builder.CreateCall(call_log, args);
             }
           }
 
           // Insert call to func_end_logger() before RET
-          if (auto* ret = dyn_cast<ReturnInst>(&I)) {
+          else if (auto* ret = dyn_cast<ReturnInst>(&I)) {
             builder.SetInsertPoint(ret);
             
             Value* func_name = builder.CreateGlobalStringPtr(F.getName());
-            Value* args[] = {func_name, insn_addr};
+            Value* args[] = {func_name, inst_addr};
             builder.CreateCall(func_end_log, args);
           }
 
-          //Insert call to bin_op_logger() after OP
-          if (auto* op = dyn_cast<BinaryOperator>(&I)) {
-            builder.SetInsertPoint(op);
+          // Insert call to bin_op_logger() after OP
+          else if (auto* bin_op = dyn_cast<BinaryOperator>(&I)) {
+            builder.SetInsertPoint(bin_op);
             builder.SetInsertPoint(&B, ++builder.GetInsertPoint());
            
-            Value* lhs = op->getOperand(0);
-            Value* rhs = op->getOperand(1);
+            Value* lhs = bin_op->getOperand(0);
+            Value* rhs = bin_op->getOperand(1);
             Value* func_name = builder.CreateGlobalStringPtr(F.getName());
-            Value* op_name = builder.CreateGlobalStringPtr(op->getOpcodeName());
-            Value* args[] = {func_name, op, lhs, rhs, op_name, insn_addr};
+            Value* bin_op_name = builder.CreateGlobalStringPtr(bin_op->getOpcodeName());
+            Value* args[] = {func_name, bin_op, lhs, rhs, bin_op_name, inst_addr};
             builder.CreateCall(bin_op_log, args);
           }
+
+          // Insert call to load_logger() after load
+          else if (auto* load = dyn_cast<LoadInst>(&I)) {
+            builder.SetInsertPoint(load);
+            builder.SetInsertPoint(&B, ++builder.GetInsertPoint());
+           
+            Value* p_operand = load->getPointerOperand();
+            Type* type = load->getPointerOperandType();
+            Value* func_name = builder.CreateGlobalStringPtr(F.getName());
+            Value* load_name = builder.CreateGlobalStringPtr(load->getOpcodeName());
+            Value* args[] = {func_name, load, p_operand, load_name, inst_addr};
+            builder.CreateCall(load_log, args);
+          }
+
+          // Insert call to store_logger() after store
+          else if (auto* store = dyn_cast<StoreInst>(&I)) {
+            builder.SetInsertPoint(store);
+            builder.SetInsertPoint(&B, ++builder.GetInsertPoint());
+
+            Value* v_operand = store->getValueOperand();
+            Value* p_operand = store->getPointerOperand();
+            Type* type = store->getPointerOperandType();
+            Value* func_name  = builder.CreateGlobalStringPtr(F.getName());
+            Value* store_name = builder.CreateGlobalStringPtr(store->getOpcodeName());
+            Value* args[] = {func_name, v_operand, p_operand, store_name, inst_addr};
+            builder.CreateCall(store_log, args);
+          }
+
+          else if (auto* cast = dyn_cast<CastInst>(&I)) {
+            builder.SetInsertPoint(cast);
+            builder.SetInsertPoint(&B, ++builder.GetInsertPoint());
+
+            Value* operand = cast->getOperand(0);
+            Type* src_type = cast->getSrcTy();
+            Type* dest_type = cast->getDestTy();
+            Value* func_name = builder.CreateGlobalStringPtr(F.getName());
+            Value* cast_name = builder.CreateGlobalStringPtr(cast->getOpcodeName());
+            Value* args[] = {func_name, cast, operand, cast_name, inst_addr};
+            builder.CreateCall(cast_log, args);
+          }
+
+          else {
+            outs() << I.getOpcodeName() << "\n";
+          }
+
         }
       }
 
--- IR_Collect/loggers.c
+++ IR_Collect/loggers.c
@@ -1,17 +1,29 @@
 #include <stdio.h>
 
-void func_start_logger (char* func){
-    printf("-- START OF '%s'\n", func);
+void func_start_logger (char* func_name){
+    printf("START OF '%s'\n", func_name);
 }
 
-void func_end_logger (char* func, long int val_id){
-    printf("-- END OF '%s' {%ld}\n", func, val_id);
+void func_end_logger (char* func_name, long int val_id){
+    printf("END OF '%s' {%ld}\n", func_name, val_id);
 }
 
 void call_logger (char* callee, char* caller, long int val_id){
     printf("   CALL '%s' -> '%s' {%ld}\n", callee, caller, val_id);
 }
 
-void bin_op_logger (char* func, int val, int arg0, int arg1, char* op, long int val_id){
-    printf("   %d = %d %s %d {%ld} in '%s'\n", val, arg0, op, arg1, val_id, func);
+void bin_op_logger (char* func_name, int val, int arg0, int arg1, char* bin_op_name, long int val_id){
+    printf("   %d = %d %s %d {%ld} in '%s'\n", val, arg0, bin_op_name, arg1, val_id, func_name);
+}
+
+void load_logger (char* func_name, int val, int p_operand, char* load_name, long int val_id){
+    printf("   %d = %s %d {%ld} in '%s'\n", val, load_name, p_operand, val_id, func_name);
+}
+
+void store_logger (char* func_name, int v_operand, int p_operand, char* store_name, long int val_id){
+    printf("   %s %d %d {%ld} in '%s'\n", store_name, v_operand , p_operand, val_id, func_name);
+}
+
+void cast_logger (char* func_name, int val, int operand, char* cast_name, long int val_id){
+    printf("   %d = %s %d {%ld} in '%s'\n", val, cast_name, operand, val_id, func_name);
 }
